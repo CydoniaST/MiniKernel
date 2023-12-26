554589ec25043551160870e536361dd9784905f1
VicenteMoya <131283535+VicenteMoya@users.noreply.github.com>
2023-12-26 19:39:56 +0100

    Update llamsis.h

--- minikernel/minikernel/include/llamsis.h
+++ minikernel/minikernel/include/llamsis.h
@@ -1,9 +1,9 @@
 /*
  *  minikernel/kernel/include/llamsis.h
  *
- *  Minikernel. Versión 1.0
+ *  Minikernel. VersiÃ³n 1.0
  *
- *  Fernando Pérez Costoya
+ *  Fernando PÃ©rez Costoya
  *
  */
 
@@ -19,11 +19,17 @@
 #define _LLAMSIS_H
 
 /* Numero de llamadas disponibles */
-#define NSERVICIOS 3
+#define NSERVICIOS 10
 
 #define CREAR_PROCESO 0
 #define TERMINAR_PROCESO 1
 #define ESCRIBIR 2
+#define OBTENER_ID 3
+#define DORMIR 4
+#define CREAR_MUTEX 5
+#define ABRIR_MUTEX 6
+#define LOCK 7
+#define UNLOCK 8
+#define CERRAR_MUTEX 9
 
 #endif /* _LLAMSIS_H */
-
